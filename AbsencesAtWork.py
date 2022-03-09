1c159a88ba440c04ccfd53ab2bca1457e443a245
samyolk <83557891+samyolk@users.noreply.github.com>
2022-03-10 10:54:37 +1300

    Add files via upload

--- AbsencesAtWork.py
+++ AbsencesAtWork.py
@@ -23,15 +23,20 @@ while name != "$":
     if name != "$":
         days_absent.append(int_checker("Enter amount of days absent: "))
         names_list.append(name)
-
-print("\nAverage absences this year:", sum(days_absent) / len(days_absent))
+average_absences = sum(days_absent) / len(days_absent)
+print("\nAverage absences this year:", average_absences)
 print("Person with most days absent:",
       names_list[days_absent.index(max(days_absent))])
 while item < len(days_absent):
     if days_absent[item] == 0:
         no_absences.append(names_list[item])
-    print(item)
     item += 1
+item = 0
 no_absences.sort()
-print(no_absences)
-
+print("People with no absences (alphabetical order): ", no_absences)
+while item < len(days_absent):
+    if days_absent[item] > average_absences:
+        above_average.append(names_list[item])
+    item += 1
+above_average.sort()
+print("People above the average number of absence days: ", above_average)
